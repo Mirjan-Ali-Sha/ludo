1bfadc02100c8a2e65b81c434b1500ab4ab6c9a4
Mirjan Ali Sha <32786680+Mirjan-Ali-Sha@users.noreply.github.com>
2026-04-12 15:25:57 +0530

    keep lagecy codes as well ...

---
+++ lagecy-code/sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'ludo-universe-v7';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Ludo_App-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((networkResponse) => {
+            if (!networkResponse || networkResponse.status !== 200) {
+              return networkResponse;
+            }
+
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseToCache));
+            return networkResponse;
+          })
+          .catch(() => caches.match('./index.html'));
+      })
+  );
+});
+
